30d1c1d863a1ca0c60c2ff88ea048ebe3d1b42f3
Antony Denyer <antonydenyer@gmail.com>
2013-08-03 20:44:17 +0100

    supervisor contention

--- fabfile.py
+++ fabfile.py
@@ -8,5 +8,6 @@ def deploy():
     print "ENV %s" %(env.hosts)
     run("cd ~/webapps/techbikers.com/; git pull")
     run("cd ~/webapps/techbikers.com/; sed 's/^DEBUG = True$/DEBUG = False/' -i techbikers/settings.py")
+    sudo("supervisorctl stop techbikers.com")
     sudo("pkill gunicorn")
     sudo("supervisorctl start techbikers.com")
