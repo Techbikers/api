9f9359fe02be180854223b7f3f67c7548767417d
Antony Denyer <antonydenyer@gmail.com>
2013-08-03 20:47:34 +0100

    kill django and gunicorn, not ideal for multi tennant

--- fabfile.py
+++ fabfile.py
@@ -9,5 +9,6 @@ def deploy():
     run("cd ~/webapps/techbikers.com/; git pull")
     run("cd ~/webapps/techbikers.com/; sed 's/^DEBUG = True$/DEBUG = False/' -i techbikers/settings.py")
     sudo("supervisorctl stop techbikers.com")
+    sudo("pkill django")
     sudo("pkill gunicorn")
     sudo("supervisorctl start techbikers.com")
