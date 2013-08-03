ca9053baf89303bbb4b7c44ce160f421e3374460
Antony Denyer <antonydenyer@gmail.com>
2013-08-03 21:23:17 +0100

    newrelic config for app monitoring

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
   set -e
   ROOT=/home/django/webapps/techbikers.com
+  NEW_RELIC_CONFIG_FILE=$ROOT/newrelic.ini
+  export NEW_RELIC_CONFIG_FILE
   LOGFILE=$ROOT/log/gunicorn.log
   LOGDIR=$(dirname $LOGFILE)
   NUM_WORKERS=3
@@ -9,6 +11,6 @@
   GROUP=django
   cd $ROOT
   test -d $LOGDIR || mkdir -p $LOGDIR
-  gunicorn_django -w $NUM_WORKERS \
+  newrelic-admin run-program gunicorn_django -w $NUM_WORKERS \
     --user=$USER --group=$GROUP --log-level=debug \
     --log-file=$LOGFILE 2>>$LOGFILE
