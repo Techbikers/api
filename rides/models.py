4f0ea8f1c477d1f84dce1b8df203bbbcc9d41770
Michael Willmott <michael@knodium.com>
2013-08-04 18:55:31 +0100

    Allowed some fields on ride model to be blank and/or NULL. Added the price field to the ride model as well.
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- rides/models.py
+++ rides/models.py
@@ -4,16 +4,19 @@ from django.contrib.auth.models import User
 class Ride(models.Model):
     name           = models.CharField(max_length = '255')
     slug           = models.CharField(max_length = '255', unique = True)
-    description    = models.TextField()
+    description    = models.TextField(blank = True)
     start_location = models.CharField(max_length = '255')
     end_location   = models.CharField(max_length = '255')
     start_date     = models.DateTimeField()
     end_date       = models.DateTimeField()
+
+    # Details to sign up riders
     rider_capacity = models.IntegerField(default = 0)
+    price          = models.IntegerField(default = 0)
 
     # Linked Entities
-    chapter        = models.ForeignKey('chapters.Chapter')
-    riders         = models.ManyToManyField(User)
+    chapter        = models.ForeignKey('chapters.Chapter', null=True, blank=True)
+    riders         = models.ManyToManyField(User, null=True, blank=True)
 
     class Meta:
         db_table    = 'rides'
