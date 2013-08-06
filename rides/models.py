9719178c34ba71edc2dc2c9c7343601fef8333ad
Michael Willmott <michael@knodium.com>
2013-08-07 00:00:34 +0100

    Given the signup date field in the RideRiders model a default datetime of now.
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- rides/models.py
+++ rides/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -30,7 +31,7 @@ class RideRiders(models.Model):
     ride = models.ForeignKey(Ride)
 
     # Other information we might want to know about a user signing up to a ride
-    signup_date = models.DateTimeField()
+    signup_date = models.DateTimeField(default=datetime.now())
     pending     = models.BooleanField() # is the spot reserved for them before paying?
     paid        = models.BooleanField() # have they completed payment for the ride?
     sale        = models.ForeignKey(Sale, blank=True, null=True) # what is the sale that this transaction happened through?
