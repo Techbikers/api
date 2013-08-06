b963144aafc0fc0562b2cf6ef47e3b2f8e13c35d
Michael Willmott <michael@knodium.com>
2013-08-06 23:56:26 +0100

    Added the RideRiders model to the admin interface.
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- techbikers/admin.py
+++ techbikers/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from riders.models import RiderProfile
-from rides.models import Ride
+from rides.models import Ride, RideRiders
 from chapters.models import Chapter
 from sales.models import Sale
 
 admin.site.register(RiderProfile)
 admin.site.register(Ride)
+admin.site.register(RideRiders)
 admin.site.register(Chapter)
 admin.site.register(Sale)
