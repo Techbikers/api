7fb22b5f6237348c1b876489d649fefecea27981
Michael Willmott <michael@knodium.com>
2013-08-09 12:54:11 +0100

    Hardcoded signup link to the ride for this first run of the site.
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- techbikers/views.py
+++ techbikers/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from rides.models import Ride
 
 
 def index(request):
     return render(request, 'index.html')
 
 def the_ride(request):
-    return render(request, 'the_ride.html')
+    # Get the ride - harcoded for the first run of this site.
+    ride = Ride.objects.get(id = 1)
+    return render(request, 'the_ride.html', {'ride': ride})
 
 def the_charity(request):
     return render(request, 'the_charity.html')
