daf61e7322ffeb8ab7849fc60c3b84248d43ac04
Michael Willmott <michael@knodium.com>
2013-08-14 12:47:22 +0100

    Added password reset system.
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- riders/urls.py
+++ riders/urls.py
@@ -8,3 +8,20 @@ urlpatterns = patterns('riders.views',
     url(r'^(?P<id>(\d+))/$', 'profile'),
     url(r'^(?P<id>(\d+))/(?P<slug>(.+))/$', 'profile'),
 )
+
+urlpatterns += patterns('',
+    url(r'^password/reset/$', 'django.contrib.auth.views.password_reset', {
+        'post_reset_redirect' : '/riders/password/reset/done/',
+        'template_name': 'riders/resetpassword.html'
+    }, name='reset_password'),
+    url(r'^password/reset/done/$', 'django.contrib.auth.views.password_reset_done', {
+        'template_name': 'riders/resetpassword_done.html'
+    }),
+    url(r'^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {
+        'post_reset_redirect' : '/riders/password/done/',
+        'template_name': 'riders/resetpassword_confirm.html'
+    }),
+    url(r'^password/done/$', 'django.contrib.auth.views.password_reset_complete', {
+        'template_name': 'riders/resetpassword_complete.html'
+    }),
+)
