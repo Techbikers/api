b87ac7e1ac60133c48b215ade27ef8f9ddecf907
Michael Willmott <michael@knodium.com>
2013-08-15 11:43:07 +0100

    Bug fix
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- sales/views.py
+++ sales/views.py
@@ -26,7 +26,7 @@ def checkout(request):
 
         if ride_rider:
             return render(request, 'sales/checkout.html', {'ride': ride, 'signed_up': True})
-        elif ride.spaces_left == 0:
+        elif ride.spaces_left < 1:
             # If the ride is full then don't let them pay for it
             return render(request, 'sales/checkout.html', {'ride': ride, 'full': True})
         else:
