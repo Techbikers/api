1dbb840e53daaaf961363800baf33bb86b01f0b3
Michael Willmott <michael@knodium.com>
2014-08-05 17:31:03 +0100

    Add url routing and views for ride details page.
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- rides/urls.py
+++ rides/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('rides.views',
     url(r'^$', 'index', name='rides'),
-    url(r'^(?P<slug>(.+))/$', 'details'),
+    url(r'^(?P<id>(\d+))/$', 'details'),
+    url(r'^(?P<id>(\d+))/(?P<slug>(.+))/$', 'details'),
 )
--- rides/views.py
+++ rides/views.py
@@ -12,7 +12,20 @@ def index(request):
     ride = Ride.objects.get(id = 2)
     return render(request, 'rides/index.html', {'ride': ride})
 
-def details(request, slug = None):
-    #ride = Ride.objects.get(slug = slug)
-    #return render(request, 'rides/details.html', {'ride': ride})
-    raise Http404
+
+def details(request, id, slug = None):
+    # Try and get the ride details from the id
+    try:
+        ride = Ride.objects.get(id = id)
+    except Ride.DoesNotExist:
+        raise Http404
+
+    # Check if the name is in the url, redirect if not
+    if not slug:
+        return redirect("/rides/%s/%s" % (ride.id, ride.slug))
+
+    # Check if the title is the right one, throw error if not to stop bad linking
+    if slug != ride.slug:
+        raise Http404
+
+    return render(request, 'rides/details.html', {'ride': ride})
