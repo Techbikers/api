c323d5772bcd167128a0ba153b367f29efe5679c
Michael Willmott <michael@knodium.com>
2014-08-11 10:41:34 +0100

    Reverse the no_chrome hack that makes all login redirects no_chrome
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- sales/views.py
+++ sales/views.py
@@ -9,7 +9,7 @@ from sales.forms import RideCheckoutForm
 
 import stripe
 
-@login_required(login_url='/riders/register?no_chrome')
+@login_required()
 def checkout(request, ride_id):
     try:
         # Get the ride info
