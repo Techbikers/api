0b5a17e08d5f3207e2c98c122c52741234a0d641
Michael Willmott <michael@knodium.com>
2014-08-18 11:32:04 +0100

    Remove the static pages as we now use flatpages in the db.
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- techbikers/views.py
+++ techbikers/views.py
@@ -4,12 +4,3 @@ from rides.models import Ride
 
 def index(request):
     return render(request, 'index.html')
-
-def the_charity(request):
-    return render(request, 'the_charity.html')
-
-def about(request):
-    return render(request, 'about.html')
-
-def sponsors(request):
-	return render(request, 'sponsors.html')
