d370a03a2defefc764280b8de9929efaaae02a72
Michael Willmott <michael@knodium.com>
2015-06-08 14:46:32 +0100

    Make the charge method on the sales model generic
    As we now want to be able to charge for things other than just a ride fee we want to make the charge method generic rather than linked to a specific object type. We now explicitly pass through the parameters needed to create a charge with Stripe and then create an associated sale record.
    
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- server/core/models/sales.py
+++ server/core/models/sales.py
@@ -21,34 +21,44 @@ class Sale(models.Model):
         return self.charge_id
 
     @classmethod
-    def charge(cls, user, ride, token):
+    def charge(cls, token, key, price, currency, description):
         """
-        Takes a the price and a Stripe token.
+        Charges a users card using the Stripe token obtained and
+        details of the transaction. If successful we pass back a
+        sale object with the transaction details.
+
+        price: to be sent through in cents/pennies
+            i.e. for GBP 3 we pass through 300
+
+        key: this is the private Stripe key
+
+        currency: a string of the 3 letter ISO code
+            e.g. gbp or eur
+
+        description: this will be sent to the customer so they
+            can identify the transaction
  
         Raises a stripe.CardError on errors.
         """
 
         instance = cls()
-
-        stripe.api_key = ride.chapter.get_priv_key()
+        stripe.api_key = key
 
         try:
             response = stripe.Charge.create(
-                amount = int(ride.price * 100), # in cents
-                currency = ride.currency,
-                card = token,
-                receipt_email = user.email,
-                description = "{}: {}".format(ride.name, user.email))
-        except stripe.CardError:
-            raise # Just be explicit about what we are raising
+                amount=price,
+                currency=currency,
+                source=token,
+                description=description)
+        except Exception, e:
+            raise e
 
         instance.charge_id = response.id
         instance.amount = response.amount
         instance.currency = response.currency
         instance.livemode = response.livemode
         instance.card = response.card.id
-        instance.rider_id = user.id
- 
+
         return instance
         
     class Meta:
