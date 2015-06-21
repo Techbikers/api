c3e9f04354ae02dfa74e2cdc5165d619c28a774b
Michael Willmott <michael@knodium.com>
2015-06-22 00:42:30 +0100

    Add CSRF token to the header for anonymous users
    There are some times where we might want to protect against CSRF attacks when we don't have the auth token in the header. For these times we now inject a CSRF token into the X-CSRFToken header.
    
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- client/hooks/httpHooks.js
+++ client/hooks/httpHooks.js
@@ -10,3 +10,30 @@ Marty.HttpStateSource.addHook({
     }
   }
 });
+
+Marty.HttpStateSource.addHook({
+  id: 'addCSRFToken',
+
+  before(req) {
+
+    function getCookie(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i];
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
+        }
+      }
+      return cookieValue;
+    }
+
+    if (!this.app.authStore.isLoggedIn()) {
+      req.headers['X-CSRFToken'] = getCookie('csrftoken')
+    }
+  }
+});
