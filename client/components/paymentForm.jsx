0e0cc8f86ce7fe2f53655022ced5b64d2b4d181f
Michael Willmott <michael@knodium.com>
2015-07-10 14:24:16 +0100

    Update payment form
    This updates the styling of the payment form, adds an option to display an "amount" input and set a minimum value on this. It also introduces the payment utils which we use to formate and validate parts of the payment form.
    
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- client/components/paymentForm.jsx
+++ client/components/paymentForm.jsx
@@ -1,49 +1,113 @@
 import _ from "lodash";
 import React, { Component } from "react";
-import forms, { Form, RenderForm } from 'newforms';
+import forms, { Form, RenderForm, TextInput } from 'newforms';
+import PaymentUtils from "../utils/paymentUtils";
 
 import FormField from "./formField.jsx";
-
-const PaymentDetailsForm = Form.extend({
-  number: forms.CharField({
-    initial: "",
-    label: "XXXX XXXX XXXX XXXX",
-    maxLength: 16
-  }),
-  cvc: forms.CharField({
-    label: "CVC",
-    maxLength: 3
-  }),
-  exp_month: forms.CharField({
-    label: "MM",
-    maxLength: 2
-  }),
-  exp_year: forms.CharField({
-    label: "YYYY",
-    maxLength: 4
-  }),
-  name: forms.CharField({
-    label: "Name on card"
-  }),
-  terms: forms.BooleanField({
-    label: 'I have read and agree to the Terms and Conditions'
-  })
-});
 
 class PaymentForm extends Component {
   static propTypes = {
     onSubmit: React.PropTypes.func.isRequired,
+    customAmount: React.PropTypes.boolean
   }
 
   static defaultProps = {
-    submitText: "Submit"
+    submitText: "Submit",
+    customAmount: true,
+    minAmount: 0
   }
 
   constructor(options) {
     super(options);
     this.state = {
-      form: new PaymentDetailsForm({onChange: this.onFormChange.bind(this)})
+      form: this.paymentDetailsForm()
     }
+  }
+
+  paymentDetailsForm() {
+    const PaymentDetailsForm = Form.extend({
+      amount: forms.DecimalField({
+        label: this.props.minAmount,
+        required: this.props.customAmount,
+        initial: this.props.minAmount,
+        minValue: this.props.minAmount,
+        maxDecimalPlaces: 2
+      }),
+      number: forms.CharField({
+        initial: "",
+        label: "•••• •••• •••• ••••",
+        widgetAttrs: {
+          onKeyPress: (event) => {
+            PaymentUtils.restrictNumeric(event);
+            PaymentUtils.restrictCardNumber(event);
+            PaymentUtils.formatCardNumber(event);
+          },
+          onKeyDown: (event) => {
+            PaymentUtils.formatBackCardNumber(event);
+          },
+          onChange: (event) => {
+            PaymentUtils.reFormatCardNumber(event);
+          },
+          onPaste: (event) => {
+            PaymentUtils.reFormatCardNumber(event);
+          },
+          onInput: (event) => {
+            PaymentUtils.reFormatCardNumber(event);
+          }
+        }
+      }),
+      cvc: forms.CharField({
+        label: "•••",
+        maxLength: 4,
+        widgetAttrs: {
+          onKeyPress: (event) => {
+            PaymentUtils.restrictNumeric(event);
+            PaymentUtils.restrictCVC(event);
+          },
+          onPaste: (event) => {
+            PaymentUtils.reFormatCVC(event);
+          },
+          onChange: (event) => {
+            PaymentUtils.reFormatCVC(event);
+          },
+          onInput: (event) => {
+            PaymentUtils.reFormatCVC(event);
+          }
+        }
+      }),
+      exp: forms.CharField({
+        label: "MM / YY",
+        widgetAttrs: {
+          onKeypress: (event) => {
+            PaymentUtils.restrictNumeric(event);
+            PaymentUtils.restrictExpiry(event);
+            PaymentUtils.formatExpiry(event);
+            PaymentUtils.formatForwardSlashAndSpace(event);
+            PaymentUtils.formatForwardExpiry(event);
+          },
+          onKeyDown: (event) => {
+            PaymentUtils.formatBackExpiry(event);
+          },
+          onChange: (event) => {
+            PaymentUtils.reFormatExpiry(event);
+          },
+          onInput: (event) => {
+            PaymentUtils.reFormatExpiry(event);
+          }
+        }
+      }),
+      name: forms.CharField({
+        label: "Name on card"
+      }),
+      clean() {
+        if (this.cleanedData.exp) {
+          let exp = PaymentUtils.getCardExpiryVal(this.cleanedData.exp);
+          this.cleanedData["exp_month"] = exp.month;
+          this.cleanedData["exp_year"] = exp.year;
+        }
+      }
+    });
+    return new PaymentDetailsForm({onChange: this.onFormChange.bind(this)});
   }
 
   onFormChange() {
@@ -53,21 +117,27 @@ class PaymentForm extends Component {
   onFormSubmit(e) {
     e.preventDefault();
     let form = this.state.form;
-    if (form.validate()) {
-      this.props.onSubmit(form.cleanedData)
-    }
+    if (form.validate())
+      this.props.onSubmit(form.cleanedData);
   }
 
   render() {
     let fields = this.state.form.boundFieldsObj();
-    let cardType = Stripe.card.cardType(this.state.form.data.number).toLowerCase().replace(" ", "-");
     return (
       <form className="payment-form">
+        {this.props.customAmount ?
+          <div className="payment-form--amount row">
+            <h2>Amount: £<FormField field={fields.amount} /></h2>
+          </div> : ""}
+
         <div className="payment-form--credit-card">
+          <div className="row payment-form--title">
+            <h3>Card Details</h3>
+          </div>
           <div className="row">
             <div className="longNumber">
               <FormField field={fields.number} />
-              <span className={"card-type " + cardType}></span>
+              <span className={"card-type " + PaymentUtils.getCardType(this.state.form.data.number)}></span>
             </div>
             <FormField className="cvc" field={fields.cvc} />
           </div>
@@ -75,9 +145,8 @@ class PaymentForm extends Component {
           <div className="row">
             <FormField className="nameOnCard" field={fields.name} />
 
-            <div className="date">
-              <FormField className="month" field={fields.exp_month} />
-              <FormField className="year" field={fields.exp_year} />
+            <div className="exp">
+              <FormField className="month" field={fields.exp} />
             </div>
           </div>
         </div>
---
+++ client/utils/paymentUtils.js
@@ -0,0 +1,492 @@
+import _ from "lodash";
+import $ from "jquery";
+
+const defaultFormat = /(\d{1,4})/g;
+const cards = [
+  {
+    type: 'visaelectron',
+    pattern: /^4(026|17500|405|508|844|91[37])/,
+    format: defaultFormat,
+    length: [16],
+    cvcLength: [3],
+    luhn: true
+  }, {
+    type: 'maestro',
+    pattern: /^(5(018|0[23]|[68])|6(39|7))/,
+    format: defaultFormat,
+    length: [12, 13, 14, 15, 16, 17, 18, 19],
+    cvcLength: [3],
+    luhn: true
+  }, {
+    type: 'forbrugsforeningen',
+    pattern: /^600/,
+    format: defaultFormat,
+    length: [16],
+    cvcLength: [3],
+    luhn: true
+  }, {
+    type: 'dankort',
+    pattern: /^5019/,
+    format: defaultFormat,
+    length: [16],
+    cvcLength: [3],
+    luhn: true
+  }, {
+    type: 'visa',
+    pattern: /^4/,
+    format: defaultFormat,
+    length: [13, 16],
+    cvcLength: [3],
+    luhn: true
+  }, {
+    type: 'mastercard',
+    pattern: /^(5[0-5]|2[2-7])/,
+    format: defaultFormat,
+    length: [16],
+    cvcLength: [3],
+    luhn: true
+  }, {
+    type: 'amex',
+    pattern: /^3[47]/,
+    format: /(\d{1,4})(\d{1,6})?(\d{1,5})?/,
+    length: [15],
+    cvcLength: [3, 4],
+    luhn: true
+  }, {
+    type: 'dinersclub',
+    pattern: /^3[0689]/,
+    format: /(\d{1,4})(\d{1,6})?(\d{1,4})?/,
+    length: [14],
+    cvcLength: [3],
+    luhn: true
+  }, {
+    type: 'discover',
+    pattern: /^6([045]|22)/,
+    format: defaultFormat,
+    length: [16],
+    cvcLength: [3],
+    luhn: true
+  }, {
+    type: 'unionpay',
+    pattern: /^(62|88)/,
+    format: defaultFormat,
+    length: [16, 17, 18, 19],
+    cvcLength: [3],
+    luhn: false
+  }, {
+    type: 'jcb',
+    pattern: /^35/,
+    format: defaultFormat,
+    length: [16],
+    cvcLength: [3],
+    luhn: true
+  }
+];
+
+const PaymentUtils = {
+  cardFromNumber(num) {
+    num = (num + '').replace(/\D/g, '');
+    return _.find(cards, card => {
+      return card.pattern.test(num)
+    });
+  },
+
+  luhnCheck(num) {
+    var digit, digits, odd, sum, _i, _len;
+    odd = true;
+    sum = 0;
+    digits = (num + '').split('').reverse();
+    for (_i = 0, _len = digits.length; _i < _len; _i++) {
+      digit = digits[_i];
+      digit = parseInt(digit, 10);
+      if ((odd = !odd)) {
+        digit *= 2;
+      }
+      if (digit > 9) {
+        digit -= 9;
+      }
+      sum += digit;
+    }
+    return sum % 10 === 0;
+  },
+
+  hasTextSelected($target) {
+    var _ref;
+    if (($target.prop('selectionStart') != null) && $target.prop('selectionStart') !== $target.prop('selectionEnd')) {
+      return true;
+    }
+    if ((typeof document !== "undefined" && document !== null ? (_ref = document.selection) != null ? _ref.createRange : void 0 : void 0) != null) {
+      if (document.selection.createRange().text) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  reFormatNumeric(e) {
+    var $target, value;
+    $target = $(e.currentTarget);
+    value = $target.val();
+    value = value.replace(/\D/g, '');
+    return $target.val(value);
+  },
+
+  reFormatCardNumber(e) {
+    var $target, value;
+    $target = $(e.currentTarget);
+    value = $target.val();
+    value = this.getFormattedCardNumber(value);
+    return $target.val(value);
+  },
+
+  formatCardNumber(e) {
+    var $target, card, digit, length, re, upperLength, value;
+    digit = String.fromCharCode(e.which);
+    if (!/^\d+$/.test(digit)) {
+      return;
+    }
+    $target = $(e.currentTarget);
+    value = $target.val();
+    card = this.cardFromNumber(value + digit);
+    length = (value.replace(/\D/g, '') + digit).length;
+    upperLength = 16;
+    if (card) {
+      upperLength = card.length[card.length.length - 1];
+    }
+    if (length >= upperLength) {
+      return;
+    }
+    if (($target.prop('selectionStart') != null) && $target.prop('selectionStart') !== value.length) {
+      return;
+    }
+    if (card && card.type === 'amex') {
+      re = /^(\d{4}|\d{4}\s\d{6})$/;
+    } else {
+      re = /(?:^|\s)(\d{4})$/;
+    }
+    if (re.test(value)) {
+      e.preventDefault();
+      return setTimeout(function() {
+        return $target.val(value + ' ' + digit);
+      });
+    } else if (re.test(value + digit)) {
+      e.preventDefault();
+      return setTimeout(function() {
+        return $target.val(value + digit + ' ');
+      });
+    }
+  },
+
+  formatBackCardNumber(e) {
+    var $target, value;
+    $target = $(e.currentTarget);
+    value = $target.val();
+    if (e.which !== 8) {
+      return;
+    }
+    if (($target.prop('selectionStart') != null) && $target.prop('selectionStart') !== value.length) {
+      return;
+    }
+    if (/\d\s$/.test(value)) {
+      e.preventDefault();
+      return setTimeout(function() {
+        return $target.val(value.replace(/\d\s$/, ''));
+      });
+    } else if (/\s\d?$/.test(value)) {
+      e.preventDefault();
+      return setTimeout(function() {
+        return $target.val(value.replace(/\d$/, ''));
+      });
+    }
+  },
+
+  reFormatExpiry(e) {
+    var $target, value;
+    $target = $(e.currentTarget);
+    value = $target.val();
+    value = this.getFormattedExpiry(value);
+    return $target.val(value);
+  },
+
+  formatExpiry(e) {
+    var $target, digit, val;
+    digit = String.fromCharCode(e.which);
+    if (!/^\d+$/.test(digit)) {
+      return;
+    }
+    $target = $(e.currentTarget);
+    val = $target.val() + digit;
+    if (/^\d$/.test(val) && (val !== '0' && val !== '1')) {
+      e.preventDefault();
+      return setTimeout(function() {
+        return $target.val("0" + val + " / ");
+      });
+    } else if (/^\d\d$/.test(val)) {
+      e.preventDefault();
+      return setTimeout(function() {
+        return $target.val("" + val + " / ");
+      });
+    }
+  },
+
+  formatForwardExpiry(e) {
+    var $target, digit, val;
+    digit = String.fromCharCode(e.which);
+    if (!/^\d+$/.test(digit)) {
+      return;
+    }
+    $target = $(e.currentTarget);
+    val = $target.val();
+    if (/^\d\d$/.test(val)) {
+      return $target.val("" + val + " / ");
+    }
+  },
+
+  formatForwardSlashAndSpace(e) {
+    var $target, val, which;
+    which = String.fromCharCode(e.which);
+    if (!(which === '/' || which === ' ')) {
+      return;
+    }
+    $target = $(e.currentTarget);
+    val = $target.val();
+    if (/^\d$/.test(val) && val !== '0') {
+      return $target.val("0" + val + " / ");
+    }
+  },
+
+  formatBackExpiry(e) {
+    var $target, value;
+    $target = $(e.currentTarget);
+    value = $target.val();
+    if (e.which !== 8) {
+      return;
+    }
+    if (($target.prop('selectionStart') != null) && $target.prop('selectionStart') !== value.length) {
+      return;
+    }
+    if (/\d\s\/\s$/.test(value)) {
+      e.preventDefault();
+      return setTimeout(function() {
+        return $target.val(value.replace(/\d\s\/\s$/, ''));
+      });
+    }
+  },
+
+  reFormatCVC(e) {
+    var $target, value;
+    $target = $(e.currentTarget);
+    value = $target.val();
+    value = value.replace(/\D/g, '').slice(0, 4);
+    return $target.val(value);
+  },
+
+  restrictNumeric(e) {
+    var input;
+    if (e.metaKey || e.ctrlKey) {
+      return true;
+    }
+    if (e.which === 32) {
+      return false;
+    }
+    if (e.which === 0) {
+      return true;
+    }
+    if (e.which < 33) {
+      return true;
+    }
+    input = String.fromCharCode(e.which);
+    if (!/[\d\s]/.test(input))
+      e.preventDefault();
+  },
+
+  restrictCardNumber(e) {
+    var $target, card, digit, value;
+    $target = $(e.currentTarget);
+    digit = String.fromCharCode(e.which);
+    if (!/^\d+$/.test(digit)) {
+      return;
+    }
+    if (this.hasTextSelected($target)) {
+      return;
+    }
+    value = ($target.val() + digit).replace(/\D/g, '');
+    card = this.cardFromNumber(value);
+    if (card) {
+      if (value.length > card.length[card.length.length - 1])
+        e.preventDefault();
+    } else {
+      if (value.length > 16)
+        e.preventDefault();
+    }
+  },
+
+  restrictExpiry(e) {
+    var $target, digit, value;
+    $target = $(e.currentTarget);
+    digit = String.fromCharCode(e.which);
+    if (!/^\d+$/.test(digit)) {
+      return;
+    }
+    if (this.hasTextSelected($target)) {
+      return;
+    }
+    value = $target.val() + digit;
+    value = value.replace(/\D/g, '');
+    if (value.length > 6) {
+      return false;
+    }
+  },
+
+  restrictCVC(e) {
+    var $target, digit, val;
+    $target = $(e.currentTarget);
+    digit = String.fromCharCode(e.which);
+    if (!/^\d+$/.test(digit)) {
+      return;
+    }
+    if (this.hasTextSelected($target)) {
+      return;
+    }
+    val = $target.val() + digit;
+    return val.length <= 4;
+  },
+
+  // VALIDATE METHODS
+  validateCardNumber(num) {
+    var card, _ref;
+    num = (num + '').replace(/\s+|-/g, '');
+    if (!/^\d+$/.test(num)) {
+      return false;
+    }
+    card = cardFromNumber(num);
+    if (!card) {
+      return false;
+    }
+    return (_ref = num.length, __indexOf.call(card.length, _ref) >= 0) && (card.luhn === false || luhnCheck(num));
+  },
+
+  validateCardExpiry(month, year) {
+    var currentTime, expiry, _ref;
+    if (typeof month === 'object' && 'month' in month) {
+      _ref = month, month = _ref.month, year = _ref.year;
+    }
+    if (!(month && year)) {
+      return false;
+    }
+    month = $.trim(month);
+    year = $.trim(year);
+    if (!/^\d+$/.test(month)) {
+      return false;
+    }
+    if (!/^\d+$/.test(year)) {
+      return false;
+    }
+    if (!((1 <= month && month <= 12))) {
+      return false;
+    }
+    if (year.length === 2) {
+      if (year < 70) {
+        year = "20" + year;
+      } else {
+        year = "19" + year;
+      }
+    }
+    if (year.length !== 4) {
+      return false;
+    }
+    expiry = new Date(year, month);
+    currentTime = new Date;
+    expiry.setMonth(expiry.getMonth() - 1);
+    expiry.setMonth(expiry.getMonth() + 1, 1);
+    return expiry > currentTime;
+  },
+
+  validateCardCVC(cvc, type) {
+    var card, _ref;
+    cvc = $.trim(cvc);
+    if (!/^\d+$/.test(cvc)) {
+      return false;
+    }
+    card = cardFromType(type);
+    if (card != null) {
+      return _ref = cvc.length, __indexOf.call(card.cvcLength, _ref) >= 0;
+    } else {
+      return cvc.length >= 3 && cvc.length <= 4;
+    }
+  },
+
+  // METHODS TO GET FORMATTED VALUES
+  getCardType(num) {
+    if (!num) {
+      return null;
+    }
+    return _.result(this.cardFromNumber(num), "type");
+  },
+
+  getCardExpiryVal(value) {
+    var month, prefix, year, _ref;
+    value = value.replace(/\s/g, '');
+    _ref = value.split('/', 2), month = _ref[0], year = _ref[1];
+    if ((year != null ? year.length : void 0) === 2 && /^\d+$/.test(year)) {
+      prefix = (new Date).getFullYear();
+      prefix = prefix.toString().slice(0, 2);
+      year = prefix + year;
+    }
+    month = parseInt(month, 10);
+    year = parseInt(year, 10);
+    return {
+      month: month,
+      year: year
+    };
+  },
+
+  getFormattedCardNumber(num) {
+    var card, groups, upperLength, _ref;
+    num = num.replace(/\D/g, '');
+    card = this.cardFromNumber(num);
+    if (!card) {
+      return num;
+    }
+    upperLength = card.length[card.length.length - 1];
+    num = num.slice(0, upperLength);
+    if (card.format.global) {
+      return (_ref = num.match(card.format)) != null ? _ref.join(' ') : void 0;
+    } else {
+      groups = card.format.exec(num);
+      if (groups == null) {
+        return;
+      }
+      groups.shift();
+      groups = $.grep(groups, function(n) {
+        return n;
+      });
+      return groups.join(' ');
+    }
+  },
+
+  getFormattedExpiry(expiry) {
+    var mon, parts, sep, year;
+    parts = expiry.match(/^\D*(\d{1,2})(\D+)?(\d{1,4})?/);
+    if (!parts) {
+      return '';
+    }
+    mon = parts[1] || '';
+    sep = parts[2] || '';
+    year = parts[3] || '';
+    if (year.length > 0) {
+      sep = ' / ';
+    } else if (sep === ' /') {
+      mon = mon.substring(0, 1);
+      sep = '';
+    } else if (mon.length === 2 || sep.length > 0) {
+      sep = ' / ';
+    } else if (mon.length === 1 && (mon !== '0' && mon !== '1')) {
+      mon = "0" + mon;
+      sep = ' / ';
+    }
+    return mon + sep + year;
+  }
+};
+
+export default PaymentUtils;
