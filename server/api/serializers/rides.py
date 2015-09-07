2a5e7bef84068859a3fe52d30664bc2a1153ae83
Michael Willmott <michael@knodium.com>
2015-09-07 15:14:21 +0100

    Get fundraising info from Just Giving when getting ride details
    While fetching the ride details using our API, we make a call to Just Giving to get the fundraising info for the associated fundraising event.
    
    Signed-off-by: Michael Willmott <michael@knodium.com>

--- server/api/serializers/rides.py
+++ server/api/serializers/rides.py
@@ -1,5 +1,5 @@
-import markdown
-from django.contrib.auth.models import User
+import requests
+from django.conf import settings
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from server.api.serializers.chapters import ChapterSerializer
@@ -9,11 +9,28 @@ from server.core.models.rides import Ride, RideRiders
 class RideSerializer(serializers.ModelSerializer):
     chapter = ChapterSerializer()
     riders = serializers.PrimaryKeyRelatedField(source='registered_riders', many=True, read_only=True)
+    fundraising = serializers.SerializerMethodField('get_event_leaderboard')
+
+    def get_event_leaderboard(self, ride):
+        if (ride.just_giving_event_id is not None):
+            try:
+                response = requests.get(
+                    '{0}/event/{1}/leaderboard'.format(settings.JUST_GIVING_API_URL, ride.just_giving_event_id),
+                    headers={
+                        'x-api-key': settings.SOCIAL_AUTH_JUSTGIVING_KEY,
+                        'x-application-key': settings.SOCIAL_AUTH_JUSTGIVING_SECRET,
+                        'Content-Type': 'application/json'
+                    })
+                return response.json()
+            except requests.exceptions.RequestException, e:
+                return {}
+        else:
+            return {}
 
     class Meta:
         model = Ride
         fields = ('id', 'name', 'slug', 'description_html', 'start_location', 'end_location', 'start_date', 'end_date',
-                  'chapter', 'rider_capacity', 'riders', 'spaces_left', 'price', 'full_cost', 'currency', 'is_over')
+                  'chapter', 'rider_capacity', 'riders', 'spaces_left', 'price', 'full_cost', 'currency', 'is_over', 'fundraising')
 
 
 class RideRiderSerializer(serializers.ModelSerializer):
