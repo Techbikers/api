d5613759caa6ee59daa33f82d63bd52b1fd72f9b
Michael Willmott <mwillmott@gmail.com>
2017-03-10 15:43:34 +0000

    Fix config listing errors
    Signed-off-by: Michael Willmott <mwillmott@gmail.com>

--- client/config/index.js
+++ client/config/index.js
@@ -1,4 +1,4 @@
-const configs = {
+const config = {
   global: {
     API_ROOT: "/api"
   },
@@ -9,19 +9,16 @@ const configs = {
   development: {
     SEGMENT_TRACKING_KEY: "D7XGqhxXUerI0Gmz1sR48pgU5mo0iBIc"
   }
-}
-
-const globalConfig = configs.global
-const envConfig = configs[process.env.NODE_ENV]
+};
 
 // Copy global and environment config into exports
-Object.assign(module.exports, globalConfig, envConfig)
+Object.assign(module.exports, config.global, config[process.env.NODE_ENV]);
 
 if (process.env.NODE_ENV === "development") {
   // Attempt to load and copy over the local config, if it exists...
   try {
-    const localConfig = require("./local")
-    Object.assign(module.exports, localConfig)
+    const localConfig = require("./local");
+    Object.assign(module.exports, localConfig);
   } catch (e) {
     // That's fine - no local config exists...
   }
