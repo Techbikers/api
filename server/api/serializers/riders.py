e46155358e3dc849b2c09f885195667fad253d04
Michael Willmott <mwillmott@gmail.com>
2017-03-30 14:40:28 +0100

    Password is not required when creating a local user
    The password field is no longer required when creating a local user. This is because authentication is handled by Auth0 so we don’t care about passwords. A password might not even exist if the user logs in using a different provider (e.g. Facebook). Not setting a password will mean that people can’t actually authenticate against the local account (not that there are any endpoints to do that anymore).
    
    Signed-off-by: Michael Willmott <mwillmott@gmail.com>

--- server/api/serializers/riders.py
+++ server/api/serializers/riders.py
@@ -83,4 +83,4 @@ class RiderSerializer(serializers.ModelSerializer):
         fields = ('id', 'email', 'password', 'name', 'first_name', 'last_name', 'avatar', 'company',
                   'website', 'twitter', 'biography', 'statement', 'donation_page', 'is_member', 'rides', 'fundraisers')
         read_only_fields = ('id', 'donation_page', 'fundraisers')
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {'password': { 'required': False, 'write_only': True }}
