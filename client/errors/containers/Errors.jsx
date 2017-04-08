d306766a085dd6465337a1815994ebf489343316
Michael Willmott <mwillmott@gmail.com>
2017-04-08 15:05:40 +0100

    Update Error styling and remove old styling
    Signed-off-by: Michael Willmott <mwillmott@gmail.com>

--- client/errors/containers/Errors.jsx
+++ client/errors/containers/Errors.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { PropTypes } from "react";
 import { connect } from "react-redux";
+import cx from "classnames";
 
 import { ErrorShape } from "techbikers/errors/shapes";
 
@@ -9,20 +10,23 @@ const mapStateToProps = (state, props) => ({
   error: state.errors[props.errorKey]
 });
 
-const Errors = ({ error }) => {
+const Errors = ({ error, full }) => {
   if (!error) {
     return null;
   }
 
   return (
     <div className={styles.Errors}>
-      {error.message}
+      <div className={cx(styles.content, { [styles.full]: full })}>
+        {error.message}
+      </div>
     </div>
   );
 };
 
 Errors.propTypes = {
-  error: ErrorShape
+  error: ErrorShape,
+  full: PropTypes.bool
 };
 
 export default connect(mapStateToProps)(Errors);
