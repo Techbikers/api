36059db7b14d5d59eb19d0dea9e4371871a2c407
Michael Willmott <mwillmott@gmail.com>
2017-04-10 17:11:10 +0100

    Show a list of rides on each chapter page
    Signed-off-by: Michael Willmott <mwillmott@gmail.com>

--- client/chapters/containers/ChapterDetails.jsx
+++ client/chapters/containers/ChapterDetails.jsx
@@ -5,15 +5,18 @@ import DocumentTitle from "react-document-title";
 import { ChapterShape } from "techbikers/chapters/shapes";
 import { getCurrentChapter } from "techbikers/chapters/selectors";
 import { fetchChapterByName } from "techbikers/chapters/actions";
+import { fetchAllRides } from "techbikers/rides/actions";
 
 import Spinner from "techbikers/components/Spinner";
+import ConnectedRideCard from "techbikers/rides/containers/ConnectedRideCard";
 
 const mapStateToProps = state => ({
   chapter: getCurrentChapter(state)
 });
 
 const mapDispatchToProps = {
-  fetchChapterByName
+  fetchChapterByName,
+  fetchAllRides
 };
 
 @connect(mapStateToProps, mapDispatchToProps)
@@ -23,12 +26,14 @@ export default class ChapterPage extends Component {
     params: PropTypes.shape({
       name: PropTypes.string.isRequired
     }),
-    fetchChapterByName: PropTypes.func.isRequired
+    fetchChapterByName: PropTypes.func.isRequired,
+    fetchAllRides: PropTypes.func.isRequired
   };
 
   componentWillMount() {
     const { params } = this.props;
     this.props.fetchChapterByName(params.name);
+    this.props.fetchAllRides(); // Bit of a hack to make sure we have the rides in the store
   }
 
   render() {
@@ -42,8 +47,17 @@ export default class ChapterPage extends Component {
           <div className="content">
             <section>
               <header>
-                <h1>{chapter.name} Chapter</h1>
+                <h1>{chapter.name} Chapter Rides</h1>
               </header>
+              <div>
+                <ul className="ride-list">
+                  {chapter.rides.map(id => (
+                    <li key={id}>
+                      <ConnectedRideCard rideId={id} />
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </section>
           </div>
         </DocumentTitle>
