9d9f6e05fd7ea8a3cc32af2cf722d60bbe408a5d
Michael Willmott <mwillmott@gmail.com>
2017-04-10 14:33:35 +0100

    Make sure the RideCard component shows the proper ride dates
    Signed-off-by: Michael Willmott <mwillmott@gmail.com>

--- client/rides/components/RideCard.jsx
+++ client/rides/components/RideCard.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router";
 
 import Timestamp from "techbikers/components/Timestamp";
 
-const RideCard = ({ id, name, slug, start_date, end_date }) => (
+const RideCard = ({ id, name, slug, startDate, endDate }) => (
   <div className="ride-card">
     <Link to={`/rides/${id}/${slug}`}>
       <header>
         <h2>{name}</h2>
-        <h4><Timestamp value={start_date} format="D MMM" /> to <Timestamp value={end_date} format="D MMM YYYY" /></h4>
+        <h4><Timestamp value={startDate} format="D MMM" /> to <Timestamp value={endDate} format="D MMM YYYY" /></h4>
       </header>
     </Link>
   </div>
@@ -18,8 +18,8 @@ RideCard.propTypes = {
   id: PropTypes.number,
   name: PropTypes.string,
   slug: PropTypes.string,
-  start_date: PropTypes.string,
-  end_date: PropTypes.string
+  startDate: PropTypes.string,
+  endDate: PropTypes.string
 };
 
 export default RideCard;
