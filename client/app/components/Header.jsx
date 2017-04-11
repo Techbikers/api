49b376bacbfb49e1988cfe97877d85b75716b17f
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-04-11 19:56:58 +0100

    Migrate Header to styled-components

--- client/app/components/Header.jsx
+++ client/app/components/Header.jsx
@@ -1,49 +1,80 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router";
+import styled from "styled-components";
 
-export default class Header extends Component {
-  static propTypes = {
-    menu: PropTypes.arrayOf(PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      route: PropTypes.string.isRequired
-    }))
-  };
+const Content = styled.header`
+  width: 800px;
+  margin: auto;
+`;
 
-  static defaultProps = {
-    menu: [{
-      title: "Rides",
-      route: "/rides"
-    }, {
-      title: "About",
-      route: "/about"
-    }, {
-      title: "Sponsors",
-      route: "/sponsors"
-    }, {
-      title: "Charity",
-      route: "/the_charity"
-    }]
-  };
+const Navigation = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  height: 140px;
+`;
 
-  render() {
-    const { menu } = this.props;
+const NavLink = styled(Link)`
+  transition: color .3s linear;
+  margin-right: 20px;
+  color: #666;
 
-    return (
-      <header role="header" className="content">
-        <nav className="topNavigation">
-          <Link id="logo" to="/">
-            <img src="/static/img/logo@2x.png" alt="TechBikers" width="125" />
-          </Link>
-
-          {menu.map(item =>
-            <Link key={item.route} to={item.route} className="menu-item" activeClassName="menu-item__active">{item.title}</Link>
-          )}
-
-          <a href="http://blog.techbikers.com/">Blog</a>
-
-          <Link className="btn btn-green donate" to="/donate">Donate!</Link>
-        </nav>
-      </header>
-    );
+  &:hover {
+    text-decoration: none;
+    ${props => props.color ? `color: ${props.color}` : ""}
   }
-}
+`;
+
+const BlogLink = styled.a`
+  transition: color .3s linear;
+  margin-right: 20px;
+  color: #666;
+
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+const MainLink = styled(NavLink)`
+  height: 140px;
+`;
+
+const Section = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export default () => (
+  <Content role="header">
+    <Navigation>
+      <Section>
+        <MainLink to="/">
+          <img src="/static/img/logo@2x.png" alt="TechBikers" width="125" />
+        </MainLink>
+
+        <NavLink to="/rides" color="#eb1c24">
+          Rides
+        </NavLink>
+
+        <NavLink to="/about" color="#4494c7">
+          About
+        </NavLink>
+
+        <NavLink to="/sponsors" color="#625398">
+          Sponsors
+        </NavLink>
+
+        <NavLink to="/the_charity" color="#76c15a">
+          Charity
+        </NavLink>
+
+        <BlogLink href="http://blog.techbikers.com/">
+          Blog
+        </BlogLink>
+      </Section>
+
+      <Section>
+        <Link className="btn btn-green" to="/donate">Donate!</Link>
+      </Section>
+    </Navigation>
+  </Content>
+);
