24b867ea19647e149929cc0ec7ede97d6a7d530d
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-04-11 18:20:37 +0100

    Make modal contents scrollable

--- client/utils/Modal.js
+++ client/utils/Modal.js
@@ -3,19 +3,23 @@ import Modal from "react-modal";
 
 export const modalStyles = {
   overlay: {
-    backgroundColor: "rgba(238, 238, 238, 0.98)"
+    backgroundColor: "rgba(238, 238, 238, 0.98)",
+    overflow: "auto",
+    display: "flex",
+    justifyContent: "center"
   },
   content: {
     backgroundColor: "transparent",
     border: "none",
     maxWidth: "800px",
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    marginBottom: "-50%",
-    transform: "translate(-50%, -50%)",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    marginTop: "auto",
+    marginBottom: "auto",
+    position: "relative",
+    overflow: "visible",
     padding: "0"
   }
 };
