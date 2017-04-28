7725bfcd5310813b6d2892bf33d1248c7274ff6f
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-04-28 17:32:26 +0100

    Convert cover image on the home page to styled-components

--- client/app/components/pages/Home.jsx
+++ client/app/components/pages/Home.jsx
@@ -1,26 +1,62 @@
 import React from "react";
 import DocumentTitle from "react-document-title";
+import styled from "styled-components";
 
 import MailchimpForm from "techbikers/components/MailchimpForm";
+
+const CoverImage = styled.div`
+  background-image: url("/static/img/background.jpg");
+  background-size: cover;
+  padding: 100px 0;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Strapline = styled.div`
+  text-shadow: 0px 0px 2px black;
+  text-transform: uppercase;
+  font-weight: bold;
+  text-align: center;
+`;
+
+const Strap1 = styled.div`
+  font-size: 130px;
+  line-height: 1;
+`;
+
+const Strap2 = styled.div`
+  font-size: 22px;
+  line-height: 1;
+`;
+
+const StrapAmp = styled.span`
+  display: inline-block;
+  font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif;
+  font-weight: normal;
+  font-style: italic;
+  font-size: 48px;
+  transform: translateY(5px);
+`;
+
+const Strap3 = styled.div`
+  font-size: 166px;
+  line-height: 1;
+`;
 
 const IndexPage = () => (
   <DocumentTitle title="Techbikers">
     <div>
-      <div className="hero">
-        <div className="content">
-          <div className="row">
+      <CoverImage>
+        <Strapline>
+          <Strap1>Drop</Strap1>
+          <Strap2>your laptop <StrapAmp>&</StrapAmp> get on your</Strap2>
+          <Strap3>bike</Strap3>
+        </Strapline>
+      </CoverImage>
 
-            <div className="strapline">
-              <span className="strap1">Drop</span>
-              <span className="strap2">your laptop <span className="strapAmp">&amp;</span> get on your</span>
-              <span className="strap3">bike</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div id="main" className="content">
-
+      <div className="content">
         <section id="mission">
           <header>
             <h1>Our Mission</h1>
