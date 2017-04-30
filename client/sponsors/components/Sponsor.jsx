f4701b36749032896d2bad0d7cccfa59d7e0fdfb
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-04-30 11:48:20 +0100

    Convert Sponsors page to styled-components

--- client/sponsors/components/Sponsor.jsx
+++ client/sponsors/components/Sponsor.jsx
@@ -1,21 +1,41 @@
 import React from "react";
+import styled from "styled-components";
 
 import { SponsorShape } from "techbikers/sponsors/shapes";
 
+const Media = styled.div`
+  overflow: hidden;
+  zoom: 1;
+`;
+
+const Bd = styled.div`
+  overflow: hidden;
+  zoom: 1;
+`;
+
+const ImageLink = styled.a`
+  float:left;
+  margin-right: 20px;
+`;
+
+const Image = styled.img`
+  display: block;
+`;
+
 const Sponsor = ({ sponsor }) => (
-  <div className="media">
-    <a href={sponsor.website} className="img" target="_blank">
-      <img src={sponsor.logo} alt={sponsor.organization} />
-    </a>
-    <div className="bd">
+  <Media>
+    <ImageLink href={sponsor.website} target="_blank">
+      <Image src={sponsor.logo} alt={sponsor.organization} />
+    </ImageLink>
+    <Bd>
       <h3><a href={sponsor.website} target="_blank">{sponsor.organization}</a></h3>
       <p>
         {sponsor.description}
         <br/>
         <a href={sponsor.facebook}>Facebook</a> | <a href={sponsor.twitter}>Twitter</a>
       </p>
-    </div>
-  </div>
+    </Bd>
+  </Media>
 );
 
 Sponsor.propTypes = {
